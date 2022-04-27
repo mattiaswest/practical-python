336d3e1fdc2eba6288fe0b61881e7e51b3658168
Mattias West <43864930+mattiaswest@users.noreply.github.com>
2022-04-27 07:43:12 +0200

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+ball_height = 100
+max_bounce = 10
+bounce = 1
+
+while bounce <= max_bounce:
+	print (ball_height)
+	ball_height = ball_height * (3/5)
+	bounce = bounce+1;
